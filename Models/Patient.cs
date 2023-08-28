2d4e5573d98beba8965b9102803e5c81ac32e3ca
kjhamlin <khamlin95@aol.com>
2023-08-28 17:11:08 -0400

    Minor error fixes, and added ER digagram

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -5,5 +5,5 @@ public class Patient
     public Guid PatientId { get; set; }
     public string Name { get; set; }
     public DateOnly BirthDate { get; set; }
-    public int PrimaryCareID { get; set; }
+    public Guid PrimaryCareID { get; set; }
 }
--- Models/Specialty.cs
+++ Models/Specialty.cs
@@ -2,6 +2,6 @@ namespace ASH_HEALTH_API;
 
 public class Specialty
 {
-    public Guid id { get; set; }
+    public int id { get; set; }
     public string specialtyName { get; set; }
 }
